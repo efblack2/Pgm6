e29c65feefa47fcfa061da6d5c0d3a6e00790503
Edgar Black <efblack2@koelsch.cs.illinois.edu>
2018-03-30 13:12:55 -0500

    running scripts updated

--- parallelVersion/MPI_sm/runTest.sh
+++ parallelVersion/MPI_sm/runTest.sh
@@ -5,12 +5,45 @@ then
   exit 1
 fi
 
-nloops=5
-np=`grep -c ^processor /proc/cpuinfo`
+nloops=3
+
+# Determining MPI implementation and binding options #
+MPI=`mpiexec --version | head -1 | awk '{print $1}' ` 
+
+if [ "$MPI" == "HYDRA" ]; then
+    echo "MPICH"
+    bindings="--bind-to socket"
+    export HYDRA_TOPO_DEBUG=1
+elif [ "$MPI" == "Intel(R)" ]; then
+    echo "Intel MPI"
+    bindings="-genv I_MPI_PIN_DOMAIN=core -genv I_MPI_PIN_ORDER=scatter -genv I_MPI_DEBUG=4"
+elif [ "$MPI" == "mpiexec" ]; then
+    echo "open-mpi"
+    bindings="--bind-to core --report-bindings"
+fi
+# end of Determining MPI implementation and binding options #
+
+npt=`grep -c ^processor /proc/cpuinfo`
+numaNodes=`lscpu | grep "NUMA node(s):" | awk '{}{print $3}{}'`
+tpc=`lscpu | grep "Thread(s) per core:" | awk '{}{print $4}{}'`
+np="$(($npt / $tpc))"
+npps="$(($np / $numaNodes))"
+npm1="$(($np - 1))"
+
+if [ -n "$PGI" ]; then
+    echo "Pgi Compiler"
+elif [ -n "$INTEL_LICENSE_FILE" ]; then
+    echo "Intel Compiler"
+    #np=15
+    #npps="$(($np / $numaNodes))"
+    #npm1="$(($np - 1))"
+else
+    echo "Gnu Compiler"
+fi
 
 
 rm -f Mpi_sm_Result.txt
-for i in  `seq 1 $np`; do
+for i in 1 `seq 2 2 $np`; do
     for j in  `seq 1 $nloops`; do
         echo number of processors: $i, run number: $j 
         mpiexec -n $i p6 $1 | grep finish >>  Mpi_sm_Result.txt
@@ -20,6 +53,6 @@ done
 mkdir -p ../../../plots/PureShareMemory/$(hostname)/$2
 cat Mpi_sm_Result.txt | awk '{}{print $6, $3}{}' | awk '{Prod[$1]++; min[$1]=Prod[$1]==1||min[$1]>$2?$2:min[$1]} END{ for (var in Prod) printf "%s processors: the min is %f\n", var,min[var]}'  | sort -n   > ../../../plots/PureShareMemory/$(hostname)/$2/p6_MPI_sm.txt
 
-rm Mpi_sm_Result.txt
+#rm Mpi_sm_Result.txt
 #rm RunHistory.dat
 
--- parallelVersion/OpenMP/runTest.sh
+++ parallelVersion/OpenMP/runTest.sh
@@ -5,12 +5,68 @@ then
   exit 1
 fi
 
-nloops=5
-np=`grep -c ^processor /proc/cpuinfo`
+nloops=3
 
+npt=`grep -c ^processor /proc/cpuinfo`
+numaNodes=`lscpu | grep "NUMA node(s):" | awk '{}{print $3}{}'`
+tpc=`lscpu | grep "Thread(s) per core:" | awk '{}{print $4}{}'`
+np="$(($npt / $tpc))"
+npps="$(($np / $numaNodes))"
+npm1="$(($np - 1))"
+
+seqArray=()
+##########################################
+for i in  `seq 0 $((npps-1))`; do
+    for j in `seq 0 $((numaNodes-1))`; do
+        seqArray[i*$numaNodes+j]=$((i+j*npps))
+    done
+done
+##########################################
+#for i in `seq 0 $((npm1))`; do
+#    seqArray[i]=$i
+#done
+##########################################
+#for i in `seq 0 2 $((npm1))`; do
+#    sequence+=$i','
+#done
+#for i in `seq 1 2 $((npm1))`; do
+#    sequence+=$i','
+#done
+##########################################
+
+#echo ${seqArray[*]}
+sequence=''
+for p in `seq 0 $((  npm1  ))`; do
+    sequence+=${seqArray[p]}','
+done
+sequence=${sequence%?}
+
+if [ -n "$PGI" ]; then
+    echo "Pgi Compiler"
+    export MP_BLIST=$sequence
+    export MP_BIND="yes"
+    #export MP_BLIST="0-$npm1"
+    echo $MP_BLIST
+elif [ -n "$INTEL_LICENSE_FILE" ]; then
+    echo "Intel Compiler"
+    #np=15
+    #npps="$(($np / $numaNodes))"
+    #npm1="$(($np - 1))"
+    export OMP_PLACES=sockets
+    export OMP_PROC_BIND=true
+    #export KMP_AFFINITY=scatter
+    # needed to use dissabled in Blue waters
+    #export KMP_AFFINITY=disabled
+else
+    echo "Gnu Compiler"
+    #export OMP_PLACES=sockets
+    export GOMP_CPU_AFFINITY=$sequence
+    export OMP_PROC_BIND=true
+    #export GOMP_CPU_AFFINITY="0-$npm1"
+fi
 
 rm -f openMpResult.txt
-for i in  `seq 1 $np`; do
+for i in 1 `seq 2 2 $np`; do
     export OMP_NUM_THREADS=$i
     for j in  `seq 1 $nloops`; do
         echo number of threads: $i, run number: $j 
@@ -21,6 +77,6 @@ done
 mkdir -p ../../../plots/PureShareMemory/$(hostname)/$2
 cat openMpResult.txt | awk '{}{print $6, $3}{}' | awk '{Prod[$1]++; min[$1]=Prod[$1]==1||min[$1]>$2?$2:min[$1]} END{ for (var in Prod) printf "%s threads: the min is %f\n", var,min[var]}'  | sort -n  > ../../../plots/PureShareMemory/$(hostname)/$2/p6_OpenMP.txt
 
-rm openMpResult.txt
+#rm openMpResult.txt
 #rm RunHistory.dat
 
