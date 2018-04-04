b1db04cf2a6c18c58cbae3b9c22519f0d9e9204d
Edgar F. Black <efblack2@illinois.edu>
2018-04-04 09:28:22 -0500

    adding creation of soft link for output file to running directory

--- parallelVersion/compileAll.sh
+++ parallelVersion/compileAll.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+rm -rf ~/RunHistory.dat
+touch ~/RunHistory.dat
 
 mkdir -p  MPI_sm/buildGnu
+ln -s ~/RunHistory.dat MPI_sm/buildGnu/RunHistory.dat
 mkdir -p  OpenMP/buildGnu
+ln -s ~/RunHistory.dat OpenMP/buildGnu/RunHistory.dat
 
 mkdir -p  MPI_sm/buildIntel
+ln -s ~/RunHistory.dat MPI_sm/buildIntel/RunHistory.dat
 mkdir -p  OpenMP/buildIntel
+ln -s ~/RunHistory.dat OpenMP/buildIntel/RunHistory.dat
 
 mkdir -p  MPI_sm/buildPgi
+ln -s ~/RunHistory.dat MPI_sm/buildPgi/RunHistory.dat
 mkdir -p  OpenMP/buildPgi
-
+ln -s ~/RunHistory.dat OpenMP/buildPgi/RunHistory.dat
 
 
 cd OpenMP/buildGnu
